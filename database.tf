296864ea09ec98f0651b43a084d0e705155e0293
MuhammadNoman-2007 <mn5492278@gmail.com>
2026-08-14 00:48:58 +0500

    New Added

--- database.tf
+++ database.tf
@@ -5,7 +5,9 @@ resource "aws_db_instance" "default" {
   engine_version       = "8.0"
   instance_class       = var.instanceslass
   username             = var.username
-  password             = vaar.password
+  password             = var.password
   parameter_group_name = "default.mysql8.0"
   skip_final_snapshot  = true
+  vpc_security_group_ids = [aws_security_group.db.id]
+  publicly_accessible = false
 }
---
+++ iam.tf
@@ -0,0 +1,19 @@
+resource "aws_iam_role" "database_ec2_role" {
+  name = "${var.env}-database-ec2-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
